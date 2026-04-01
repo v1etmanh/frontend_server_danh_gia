50524f074807f5c71d0606a73a33b2751a947cd4
LE Viet Manh <mqnyle@gmail.com>
2026-04-01 12:20:08 +0700

    remove di ung

--- src/pages/CardView.jsx
+++ src/pages/CardView.jsx
@@ -69,7 +69,7 @@ const CardView = () => {
 
         {card.allergen_summary && (
           <p className="mt-3 text-xs text-orange-600 bg-orange-50 rounded-lg px-3 py-2">
-            ⚠ Dị ứng: {card.allergen_summary}
+         
           </p>
         )}
       </div>
